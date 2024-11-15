890daf3656f0a535a83251561ed0a5c88d741c9c
TheGH0S7 <rafaeljanuario032@gmail.com>
2024-11-15 22:16:54 +0000

    Project Init

---
+++ src/test/java/dev/rafaelj13/taskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rafaelj13.taskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
